f00a0a3ecbfd2f78e5ef825124a50a293f9f4dfe
Bruno Modinuti <modinutidev@gmail.com>
2022-10-29 15:21:43 -0300

    Criado páginas

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import {Text} from 'react-native';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function Sobre() {
-  return <Text>Teste</Text>;
-}
-
-function Home() {
-  return <Text>Teste</Text>;
-}
-function Loja() {
-  return <Text>Teste</Text>;
-}
+import Carteira from './pages/Carteira';
+import Mercado from './pages/Mercado';
+import Sobre from './pages/Sobre';
 
 function icone(name, size, color) {
   return <Icon name={name} size={size} color={color} />;
@@ -29,15 +19,15 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen
           name="Carteira"
-          component={Sobre}
+          component={Carteira}
           options={{
             tabBarIcon: props =>
               icone('account-balance-wallet', props.size, props.color),
           }}
         />
         <Tab.Screen
-          name="Mercado"
-          component={Loja}
+          name="Investir"
+          component={Mercado}
           options={{
             tabBarIcon: props =>
               icone('local-grocery-store', props.size, props.color),
@@ -45,7 +35,7 @@ export default function App() {
         />
         <Tab.Screen
           name="Sobre"
-          component={Home}
+          component={Sobre}
           options={{
             tabBarIcon: props => icone('account-box', props.size, props.color),
           }}
