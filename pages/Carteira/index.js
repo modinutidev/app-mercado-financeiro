f13d86b5136088a9430fb22467ec36d48c686ff3
Bruno Modinuti <modinutidev@gmail.com>
2022-10-29 20:16:39 -0300

    Separado CSS Carteira

--- pages/Carteira/index.js
+++ pages/Carteira/index.js
@@ -1,38 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-
+import {styles} from './styles';
 import Ativo from '../../components/Ativo';
 
 export default function Carteira() {
   return (
     <SafeAreaView>
-      <View
-        style={{
-          height: 145,
-          backgroundColor: '#A01DAB',
-          borderBottomLeftRadius: 10,
-          borderBottomRightRadius: 10,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={resultado.tituloCarteira}>Minha Carteira</Text>
-        <Text style={resultado.valorCarteira}>2.800,00</Text>
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '70%',
-            justifyContent: 'space-around',
-          }}>
-          <Text style={resultado.valorResumo}>Rendimento: +8,88%</Text>
-          <Text style={resultado.valorResumo}>Vl. aplicado: 2.400,00</Text>
+      <View style={styles.containerCarteira}>
+        <Text style={styles.tituloCarteira}>Minha Carteira</Text>
+        <Text style={styles.valorCarteira}>2.800,00</Text>
+
+        <View style={styles.containerResumo}>
+          <Text style={styles.valorResumo}>Rendimento: +8,88%</Text>
+          <Text style={styles.valorResumo}>Vl. aplicado: 2.400,00</Text>
         </View>
       </View>
 
       <View style={{padding: 20}}>
-        <Text style={{fontSize: 16, fontWeight: 'bold'}}>
-          Ações em carteira:
-        </Text>
+        <Text style={styles.tituloLista}>Ações em carteira:</Text>
+
+        <Ativo />
+        <Ativo />
+        <Ativo />
+        <Ativo />
+        <Ativo />
         <Ativo />
         <Ativo />
         <Ativo />
@@ -41,32 +33,3 @@ export default function Carteira() {
     </SafeAreaView>
   );
 }
-
-function styleText(size, weight, color) {
-  const style = StyleSheet.create({
-    styleText: {
-      fontSize: size,
-      fontWeight: weight,
-      color: color,
-    },
-  });
-  return style.styleText;
-}
-
-const resultado = StyleSheet.create({
-  tituloCarteira: {
-    fontSize: 14,
-    fontWeight: '700',
-    color: 'white',
-  },
-  valorResumo: {
-    fontSize: 11,
-    fontWeight: '400',
-    color: 'white',
-  },
-  valorCarteira: {
-    fontSize: 36,
-    fontWeight: '700',
-    color: 'white',
-  },
-});
---
+++ pages/Carteira/styles.js
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  containerCarteira: {
+    height: 145,
+    backgroundColor: '#A01DAB',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tituloCarteira: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  valorResumo: {
+    fontSize: 11,
+    fontWeight: 'normal',
+    color: 'white',
+  },
+  valorCarteira: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  tituloLista: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  containerResumo: {
+    flexDirection: 'row',
+    width: '66%',
+    justifyContent: 'space-around',
+  },
+});
