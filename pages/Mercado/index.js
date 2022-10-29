25821345bae8d41f5a69393cc1afc1af350f427b
Bruno Modinuti <modinutidev@gmail.com>
2022-10-29 20:18:43 -0300

    Separado CSS Mercado

--- pages/Mercado/index.js
+++ pages/Mercado/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {styles} from './styles';
+import Ativo from '../../components/Ativo';
 
 export default function Mercado() {
-  return <></>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.tituloPagina}>Mercado</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o código do ativo..."
+      />
+      <Text style={styles.tituloLista}>Ações:</Text>
+      {/* <Text>Ops! Nenhuma ação encontrada =/</Text> */}
+      <Ativo />
+      <Ativo />
+      <Ativo />
+      <Ativo />
+      <Ativo />
+      <Ativo />
+      <Ativo />
+      <Ativo />
+      <Ativo />
+    </SafeAreaView>
+  );
 }
---
+++ pages/Mercado/styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  tituloPagina: {
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    borderRadius: 6,
+    backgroundColor: '#E4E4E4',
+    borderColor: '#D1D1D1',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    fontWeight: '500',
+    marginVertical: 10,
+  },
+  tituloLista: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
