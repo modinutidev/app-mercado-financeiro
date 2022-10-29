f3b5c65e0798a3db32777644758b0eda45a63089
Bruno Modinuti <modinutidev@gmail.com>
2022-10-29 12:34:48 -0300

    Instalado React Navigation Tabs

--- App.js
+++ App.js
@@ -3,11 +3,26 @@ import {Text} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+function Sobre() {
+  return <Text>Teste</Text>;
+}
+
+function Home() {
+  return <Text>Teste</Text>;
+}
+
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <>
-      <Text>Teste</Text>
-      <Icon name="home" size={30} color="#000000" />
-    </>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Teste" component={Sobre} />
+        <Tab.Screen name="Home" component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
