f396bbd695ef269f4ead58bb6a6adf2c153aca66
Bruno Modinuti <modinutidev@gmail.com>
2022-11-03 01:52:05 -0300

    Ajusta componente para receber dados externos

--- src/components/Ativo/index.js
+++ src/components/Ativo/index.js
@@ -1,40 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {styles} from './styles';
 
-const codigo = 'BBAC22';
-const media = 20.993;
-const lucro = 8.886;
-const valor = 20.994;
+export default function Ativo(props) {
+  const ativo = props.ativo.item;
+  const {codigo, nome, valor, quantidade, total} = ativo;
 
-export default function Ativo() {
   return (
     <View style={styles.container}>
       <View style={styles.containerInfos}>
         <View style={{flex: 1, paddingRight: 10}}>
-          <Text style={styles.labelValor}>Código:</Text>
-          <Text style={styles.valor}>{codigo}</Text>
-        </View>
-
-        <View>
-          <Text style={styles.labelValor}>Média:</Text>
-          <Text style={styles.valor}>{`R$ ${media.toFixed(2)}`}</Text>
-        </View>
-
-        <View style={{paddingHorizontal: 10}}>
-          <Text style={styles.labelValor}>Lucro:</Text>
-          <Text
-            style={[
-              styles.valor,
-              lucro > 0 ? {color: 'green'} : {color: 'red'},
-            ]}>
-            {lucro > 0 ? `+${lucro.toFixed(2)}%` : `${lucro.toFixed(2)}%`}
+          <Text style={styles.labelValor}>{codigo}</Text>
+          <Text style={styles.valor}>
+            {nome.length >= 20 ? `${nome.substr(0, 20)}...` : nome}
           </Text>
         </View>
 
         <View>
-          <Text style={styles.labelValor}>Vl. atual:</Text>
+          <Text style={styles.labelValor}>Vl. Pago:</Text>
           <Text style={styles.valor}>{`R$ ${valor.toFixed(2)}`}</Text>
+        </View>
+
+        <View style={{paddingHorizontal: 10}}>
+          <Text style={styles.labelValor}>Qt.</Text>
+          <Text style={[styles.valor]}>{`X ${quantidade}`}</Text>
+        </View>
+
+        <View>
+          <Text style={styles.labelValor}>Vl. Total:</Text>
+          <Text style={styles.valor}>{`R$ ${total.toFixed(2)}`}</Text>
         </View>
       </View>
     </View>
