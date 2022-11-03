54ff13ff5987a6f0735f809fb837f5ace5c73eb5
Bruno Modinuti <modinutidev@gmail.com>
2022-11-03 01:50:36 -0300

    Removendo arquivos desnecessarios

--- src/pages/Mercado/Detalhes/services.js
+++
@@ -1,6 +0,0 @@
-import {db} from '../../../database/DataSource';
-
-export async function comprarAtivo(ativo) {
-  const conn = await db;
-  console.log(await conn.getRepository('Ativos').save(ativo));
-}
