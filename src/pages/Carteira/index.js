8a53cca3a5b9469dec42f93b125b238ab1456006
Bruno Modinuti <modinutidev@gmail.com>
2022-11-03 01:53:38 -0300

    Adiciona flatlist para renderização, ajustes de estilo e operações no db

--- src/pages/Carteira/index.js
+++ src/pages/Carteira/index.js
@@ -1,35 +1,60 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {styles} from './styles';
 import Ativo from '../../components/Ativo';
+import {db} from '../../database/DataSource';
 
-export default function Carteira() {
+export default function Carteira(props) {
+  const [ativos, setAtivos] = useState([]);
+
+  useEffect(() => {
+    buscarAtivos();
+  }, [props]);
+
   return (
     <SafeAreaView>
       <View style={styles.containerCarteira}>
         <Text style={styles.tituloCarteira}>Minha Carteira</Text>
-        <Text style={styles.valorCarteira}>2.800,00</Text>
+        <Text style={styles.valorCarteira}>{`R$ ${totalAplicado()}`}</Text>
 
         <View style={styles.containerResumo}>
-          <Text style={styles.valorResumo}>Rendimento: +8,88%</Text>
-          <Text style={styles.valorResumo}>Vl. aplicado: 2.400,00</Text>
+          <Text style={styles.valorResumo}>Rendimento: 0,00%</Text>
+          <Text style={styles.valorResumo}>
+            Vl. aplicado: {`R$ ${totalAplicado()}`}
+          </Text>
         </View>
       </View>
 
-      <View style={{padding: 20}}>
-        <Text style={styles.tituloLista}>Ações em carteira:</Text>
-
-        <Ativo />
-        <Ativo />
-        <Ativo />
-        <Ativo />
-        <Ativo />
-        <Ativo />
-        <Ativo />
-        <Ativo />
-        <Ativo />
+      <View
+        style={{
+          padding: 20,
+          height: '100%',
+          paddingBottom: 292,
+        }}>
+        <Text style={styles.tituloLista}>Histórico de compras:</Text>
+        <FlatList
+          data={ativos}
+          renderItem={data => <Ativo ativo={data} />}
+          keyExtractor={data => data.id}
+        />
       </View>
     </SafeAreaView>
   );
+
+  async function buscarAtivos() {
+    const conn = await db;
+    const resultado = await conn.query(
+      'SELECT *, (valor * quantidade) AS total FROM ativos;',
+    );
+    setAtivos(resultado);
+  }
+
+  function totalAplicado() {
+    let total = 0;
+    ativos.forEach(item => {
+      total += item.total;
+    });
+    return total.toFixed(2);
+  }
 }
